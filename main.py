f6b69bd4cfade5b55ada09c920a2945fc96f94b1
chamomile <55081468+cat-boop@users.noreply.github.com>
2023-03-12 21:32:35 +0300

    finish lab

--- main.py
+++ main.py
@@ -1,27 +1,45 @@
-import input_reader
-import graph_drawing
+from data_io import data_reader, data_writer
+from functions.system_of_equations import SystemOfEquations 
 from methods import chord_method, secant_method, simple_iteration_method
-
+from typing import Callable
+import graph
 
 if __name__ == "__main__":
     try:
 
-        method_choice, equation, derivative_of_equation, interval, accuracy = input_reader.read_data()
-        print("a = {}, b = {}".format(interval[0], interval[1]))
+        data: dict = data_reader.read_data()
 
-        if method_choice == 1:
-            chord_method.solve_equation(equation=equation, interval=interval, accuracy=accuracy)
-        elif method_choice == 2:
-            secant_method.solve_equation(equation=equation, interval=interval, accuracy=accuracy)
-        elif method_choice == 3:
-            simple_iteration_method.solve_equation(equation=equation, derivate_of_equation=derivative_of_equation, interval=interval, accuracy=accuracy)
-        elif method_choice == 4:
-            simple_iteration_method.solve_system_of_equations(system_of_equation_with_expressed_x=derivative_of_equation, interval=interval, accuracy=accuracy)
+        interval: list[float] = data['interval']
+        equation: Callable[[float], float] = data['equation']
+        accuracy: float = data['accuracy']
 
-        if type(equation) is not list:
-            graph_drawing.plot_function(equation, -10, 10, -20, 10, 1)
+        if isinstance(equation, SystemOfEquations):
+
+            answer_data: dict = simple_iteration_method.solve_system_of_equations(system_of_equations=equation.itself_with_expessed_x, interval=interval, accuracy=accuracy)
+            
+            if answer_data != None:
+                data_writer.write_data(answer_data)
+            
+            graph.plot_system(equation.itself[0], equation.itself[1])
         else:
-            graph_drawing.plot_system(equation[0], equation[1])
+
+            equation: Callable[[float], float] = equation.itself
+
+            method: int = data['method']
+
+            answer_data: dict = None
+
+            if method == 1:
+                answer_data = chord_method.solve_equation(equation=equation, interval=interval, accuracy=accuracy)
+            elif method == 2:
+                answer_data = secant_method.solve_equation(equation=equation, interval=interval, accuracy=accuracy)
+            elif method == 3:
+                answer_data = simple_iteration_method.solve_equation(equation=equation, interval=interval, accuracy=accuracy)
+
+            if answer_data != None:
+                data_writer.write_data(answer_data)
+            
+            graph.plot_function(equation, answer_data['Ответ'], -5, 5)
 
     except Exception as e:
         print(str(e))
